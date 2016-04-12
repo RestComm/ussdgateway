8d2ef2f551718798f10b1b2cfec6772899784b12
sergey vetyutnev <serg.vetyutnev@gmail.com>
2016-04-12 18:51:44 +0200

    Fixing #27 - Need to add "ChargeInterfaceParent" referense into SipServerSbbLocalObject interface

--- core/slee/sbbs/src/main/java/org/mobicents/ussdgateway/slee/sip/SipServerSbbLocalObject.java
+++ core/slee/sbbs/src/main/java/org/mobicents/ussdgateway/slee/sip/SipServerSbbLocalObject.java
@@ -4,12 +4,13 @@
 package org.mobicents.ussdgateway.slee.sip;
 
 import org.mobicents.slee.SbbLocalObjectExt;
+import org.mobicents.ussdgateway.slee.cdr.ChargeInterfaceParent;
 import org.mobicents.ussdgateway.slee.sri.SriParent;
 
 /**
  * @author Amit Bhayani
  *
  */
-public interface SipServerSbbLocalObject extends SbbLocalObjectExt, SriParent {
+public interface SipServerSbbLocalObject extends SbbLocalObjectExt, SriParent, ChargeInterfaceParent {
 
 }
